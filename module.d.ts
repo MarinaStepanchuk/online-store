775dfb21c607232292312d634b10dba900a2a0f2
Aliaksandr Kalanda <74773439+Alex-Kalanda@users.noreply.github.com>
2022-12-30 19:24:03 +0300

    docs: test deploy

--- module.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '*.css';
-declare module '*.png';
-declare module '*.svg';
-declare module '*.jpg';
-declare module '*.json';
-declare module '*.webp';
--- webpack.prod.config.js
+++
@@ -1,8 +0,0 @@
-const { merge } = require('webpack-merge');
-const baseConfig = require('./webpack.config');
-
-const prodConfig = {
-  mode: 'production',
-};
-
-module.exports = merge(baseConfig, prodConfig);
