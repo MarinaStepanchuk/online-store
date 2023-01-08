08563cd85cbf99b1df89830b2b47c98871244441
MarinaStepanchuk <106726117+MarinaStepanchuk@users.noreply.github.com>
2023-01-08 18:22:11 +0300

    refactor: change color

--- src/components/containers/Footer/Footer.const.ts
+++ src/components/containers/Footer/Footer.const.ts
@@ -1,5 +1,5 @@
 export const LINK_TO_RSS_COURSE = 'https://rs.school/js/';
-export const YEAR_OF_DEV = '&#169; 2022';
+export const YEAR_OF_DEV = '&#169; 2023';
 export const PersonalInfo = {
   dev1: {
     fullName: 'Marina Stepanchuk',
